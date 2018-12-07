7d2fde7a0cbcc88b1e28fc720d30a6b1d29e5762
s.domroese <s.domroese@wine-dock.de>
2018-12-07 16:41:39 +0100

    add "add" page

--- src/main/java/eu/domroese/toolbox/Repository/OttTimeRepository.java
+++ src/main/java/eu/domroese/toolbox/Repository/OttTimeRepository.java
@@ -5,6 +5,8 @@ import eu.domroese.toolbox.Model.OttTime;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
