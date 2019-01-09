35a5ed8236f1ccc47e0ce05cdb85cf19489a1558
Zonkie <soenke@domroese.eu>
2019-01-09 21:17:33 +0100

    much style, such wow! cleanup templates add properties files for dev and prod add basic authentication implementation (@TODO: Implement secure user handling)

--- src/main/java/eu/domroese/toolbox/Model/Bookmark.java
+++ src/main/java/eu/domroese/toolbox/Model/Bookmark.java
@@ -1,7 +1,10 @@
 package eu.domroese.toolbox.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.*;
-
 
 @Entity
 public class Bookmark {
